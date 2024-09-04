d01e3b4f53531dea99a2febbeb4a0aca6a0f7718
gerrard205 <ucheisaac.422@gmail.com>
2024-09-04 12:45:08 +0100

    react-checkpoint

---
+++ react_project/src/players.js
@@ -0,0 +1,47 @@
+import img1 from './Assets/messi.jpeg'
+import img2 from './Assets/ronaldo.jpeg'
+import img3 from './Assets/osimhen.jpeg'
+import img4 from './Assets/mbappe.jpeg'
+
+
+
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "kano pillers",
+    nationality: "Argentina",
+    jersyNumber: 10,
+    age:30,
+    image: img1
+  },
+
+
+  {
+    name: "Christiano Ronaldo",
+    team: "Man u",
+    nationality: "Portugal",
+    jersyNumber: 7,
+    age:39,
+    image: img2
+  },
+
+  {
+    name: "Victor Osimen",
+    team: "Napoli",
+    nationality: "Nigeria",
+    jersyNumber: 9,
+    age:25,
+    image: img3
+  },
+    {
+    name: "kylian Mbappe",
+    team: "Real Madrid",
+    nationality: "France",
+    jersyNumber: 9,
+    age:25,
+    image: img4
+    }
+
+]
+
+export default players
